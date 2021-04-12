bf813628f7059edd7171ebb20d3f73e73cd9b0c1
Stuti04Dubey <75902345+Stuti04Dubey@users.noreply.github.com>
2021-04-12 21:45:19 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,54 @@
+class Game {
+  constructor(){}
+  
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+
+  play(){
+    form.hide();
+    textSize(30);
+    text("Game Start", 120,100);
+    Player.getPlayerInfo();
+
+    if (allPlayers!==undefined){
+      var display_position=130;
+      for(var plr in allPlayers){
+        if (plr ==="Player + player.index")
+        fill("red")
+        else
+          fill("black");  
+
+          display_position+=20;
+          textSize(50);
+          text(allPlayers[plr].name+":"+ allPlayers[plr.distance,120,display_position]);
+      }
+    }
+
+
+  }
+
+
+
+
+
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  start(){
+    if(gameState === 0){
+      player = new Player();
+      player.getCount();
+      form = new Form()
+      form.display();
+    }
+  }
+}
